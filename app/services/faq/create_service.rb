a5522439ecdc5ea42d6379384b7c8ff8d71de1b7
lucasbr023 <lmc3@cin.ufpe.br>
2017-05-31 22:27:31 -0300

    fix services

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -1,21 +1,30 @@
 module FaqModule
   class CreateService
     def initialize(params)
+      # TODO: identify origin and set company
       @company = Company.last
-      @question = params["question-original"]
-      @answer = params["answer-original"]
-      @hashtags = params["hashtags-original"]
+      @question = params["question.original"]
+      @answer = params["answer.original"]
+      @hashtags = params["hashtags.original"]
     end
 
+
     def call
-      Faq.transaction do
-        faq = Faq.create(question: @question, answer: @answer, company: @company)
-        return "Hashtag Obrigatória" if @hashtags == nil
-        @hashtags.split(/[\s,]+/).each do |hashtag|
-          faq.hashtag << Hashtag.create(name: hashtag)
-        end
-      end
-      "Criado com sucesso"
+      if @hashtags == nil || @hashtags == ""
+         return "Hashtag Obrigatória"
+       end
+
+       begin
+         Faq.transaction do
+           faq = Faq.create(question: @question, answer: @answer, company: @company)
+           @hashtags.split(/[\s,]+/).each do |hashtag|
+             faq.hashtags << Hashtag.create(name: hashtag)
+           end
+         end
+         "Criado com sucesso"
+       rescue
+         "Problemas na criação"
+       end
     end
   end
 end
--- app/services/faq/remove_service.rb
+++ app/services/faq/remove_service.rb
@@ -7,18 +7,22 @@ module FaqModule
     end
 
     def call
-      faq = @company.faqs.where(id: @id).last
-      return "Questão inválida, verifique o Id" if faq == nil
+      begin
+         faq = @company.faqs.find(@id)
+       rescue
+         return "Questão inválida, verifique o Id"
+       end
 
-      Faq.transaction do
-        faq.hashtag.each do |hashtag|
-          if hashtag.faqs.count <= 1
-            hashtag.delete
-          end
-        end
-        faq.delete
-        "Deletado com sucesso"
-      end
+       Faq.transaction do
+         # Deleta as tags associadas que não estejam associadas a outros faqs
+         faq.hashtags.each do |h|
+           if h.faqs.count <= 1
+             h.delete
+           end
+         end
+         faq.delete
+         "Deletado com sucesso"
+       end
     end
   end
 end
